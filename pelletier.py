8c83b2721ecf695df0e193669916017fa5ff732e
KAMRYNN PELLETIER <126613920+kamrynn-ness@users.noreply.github.com>
2023-03-07 19:35:16 +0000

    update

--- pelletier.py
+++ pelletier.py
@@ -3,12 +3,11 @@ strategy_name = 'Corners'
 strategy_description = 'Play the next open spot.'
     
 def move(player, board, score):
-  r=0
-  c=0
+  r=1
+  c=1
   if board[1][1] == ' ':
     r=1
     c=1
-
   elif board[0][2] ==' ':
     r=0
     c=2
